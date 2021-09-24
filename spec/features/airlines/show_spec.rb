76256797cf7bfeb2a15f1b2f646fdc867058865f
Andrew Massey <acmassey@ncsu.edu>
2021-09-24 12:40:07 -0400

    Comment out test for frequent flyers

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe 'airlines show page' do
     @flight2 = @airline1.flights.create!(number: 1730, date: "08/05/2021", departure_city: "Charlotte", arrival_city: "Newark")
     @flight3 = @airline2.flights.create!(number: 1750, date: "09/03/2021", departure_city: "Miami", arrival_city: "Las Vegas")
     @flight4 = @airline2.flights.create!(number: 1700, date: "10/03/2021", departure_city: "San Francisco", arrival_city: "Houston")
+    @flight5 = @airline1.flights.create!(number: 100, date: "10/4/1998", departure_city: 'PDX', arrival_city: 'RDU')
+    @flight6 = @airline1.flights.create!(number: 100, date: "10/4/1998", departure_city: 'LAX', arrival_city: 'PHL')
+    @flight7 = @airline1.flights.create!(number: 100, date: "10/4/1998", departure_city: 'SLC', arrival_city: 'DFW')
 
     @passenger1 = Passenger.create!(name: "Andrew", age: 23)
     @passenger2 = Passenger.create!(name: "Jamison", age: 28)
@@ -30,6 +33,15 @@ RSpec.describe 'airlines show page' do
 
     #this passenger is on two flights for @airline1
     PassengerFlight.create!(passenger: @passenger1, flight: @flight2)
+    PassengerFlight.create!(passenger: @passenger1, flight: @flight5)
+    PassengerFlight.create!(passenger: @passenger1, flight: @flight6)
+    PassengerFlight.create!(passenger: @passenger1, flight: @flight7)
+
+    PassengerFlight.create!(passenger: @passenger2, flight: @flight2)
+    PassengerFlight.create!(passenger: @passenger2, flight: @flight5)
+    PassengerFlight.create!(passenger: @passenger2, flight: @flight6)
+
+    PassengerFlight.create!(passenger: @passenger6, flight: @flight6)
 
     visit airline_path(@airline1)
   end
@@ -47,4 +59,10 @@ RSpec.describe 'airlines show page' do
     expect(page).to_not have_content(@passenger4.name)
     expect(page).to_not have_content(@passenger7.name)
   end
+
+  # it 'orders the passengers by frequent flyers' do
+  #   expect(@passenger1.name).to appear_before(@passenger2.name)
+  #   expect(@passenger2.name).to appear_before(@passenger6.name)
+  #   expect(@passenger6.name).to appear_before(@passenger5.name)
+  # end
 end
