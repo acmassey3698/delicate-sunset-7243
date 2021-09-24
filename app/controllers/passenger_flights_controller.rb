61fae972afd7bb9d5fa805944d29ff760667ebeb
Andrew Massey <acmassey@ncsu.edu>
2021-09-24 11:57:27 -0400

    Change routing and controller for more RESTful solution on story 2

--- app/controllers/passenger_flights_controller.rb
+++ app/controllers/passenger_flights_controller.rb
@@ -1,4 +1,4 @@
-class PassengersController < ApplicationController
+class PassengerFlightsController < ApplicationController
   def destroy
     passenger = Passenger.find(params[:id])
     flight = Flight.find(params[:flight_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :flights do
-  resources :passengers, only: :destroy
+  resources :passengers, only: :destroy, controller: :passenger_flights
 end
 
 resources :airlines, only: :show
