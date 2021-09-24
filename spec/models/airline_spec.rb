cd47d84e704f9eca5e83919bfe00031e7287e16d
Andrew Massey <acmassey@ncsu.edu>
2021-09-24 12:40:20 -0400

    Test relationship for airline having many passengers

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Airline do
   describe 'relationships' do
     it { should have_many(:flights) }
+    it { should have_many(:passengers).through(:flights)}
   end
 
   before :each do
