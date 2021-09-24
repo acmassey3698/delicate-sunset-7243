49d3d2d6cfa2563c45d7a0b0b3181c475e23bf8d
Andrew Massey <acmassey@ncsu.edu>
2021-09-24 11:46:28 -0400

    Create instance method and test for #adult_passengers

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -4,4 +4,41 @@ RSpec.describe Airline do
   describe 'relationships' do
     it { should have_many(:flights) }
   end
+
+  before :each do
+    @airline1 = Airline.create!(name: 'Delta')
+    @airline2 = Airline.create!(name: 'American')
+
+    @flight1 = @airline1.flights.create!(number: 1727, date: "08/03/2021", departure_city: "Raleigh", arrival_city: "Denver")
+    @flight2 = @airline1.flights.create!(number: 1730, date: "08/05/2021", departure_city: "Charlotte", arrival_city: "Newark")
+    @flight3 = @airline2.flights.create!(number: 1750, date: "09/03/2021", departure_city: "Miami", arrival_city: "Las Vegas")
+    @flight4 = @airline2.flights.create!(number: 1700, date: "10/03/2021", departure_city: "San Francisco", arrival_city: "Houston")
+
+    @passenger1 = Passenger.create!(name: "Andrew", age: 23)
+    @passenger2 = Passenger.create!(name: "Jamison", age: 28)
+    @passenger3 = Passenger.create!(name: "Kevin", age: 15)
+    @passenger4 = Passenger.create!(name: "Henry", age: 10)
+    @passenger5 = Passenger.create!(name: "Khoi", age: 35)
+    @passenger6 = Passenger.create!(name: "Josh", age: 40)
+    @passenger7 = Passenger.create!(name: "Sean", age: 10)
+    @passenger8 = Passenger.create!(name: "Sarah", age: 8)
+
+    PassengerFlight.create!(passenger: @passenger1, flight: @flight1)
+    PassengerFlight.create!(passenger: @passenger2, flight: @flight1)
+    PassengerFlight.create!(passenger: @passenger3, flight: @flight1)
+    PassengerFlight.create!(passenger: @passenger4, flight: @flight1)
+    PassengerFlight.create!(passenger: @passenger5, flight: @flight2)
+    PassengerFlight.create!(passenger: @passenger6, flight: @flight2)
+    PassengerFlight.create!(passenger: @passenger7, flight: @flight2)
+    PassengerFlight.create!(passenger: @passenger8, flight: @flight4)
+
+    #this passenger is on two flights for @airline1
+    PassengerFlight.create!(passenger: @passenger1, flight: @flight2)
+  end
+
+  describe 'instance methods' do
+    it '#adult_passengers' do
+      expect(@airline1.adult_passengers).to eq([@passenger1, @passenger2, @passenger5, @passenger6])
+    end
+  end
 end
