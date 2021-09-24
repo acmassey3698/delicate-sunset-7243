713e4806d10924e05972ff780311b95f00c8e85a
Andrew Massey <acmassey@ncsu.edu>
2021-09-24 11:23:51 -0400

    Create destroy action for the Passengers controller

---
+++ app/controllers/passengers_controller.rb
@@ -0,0 +1,10 @@
+class PassengersController < ApplicationController
+  def destroy
+    passenger = Passenger.find(params[:id])
+    flight = Flight.find(params[:flight_id])
+    require "pry"; binding.pry
+    pass_flight = PassengerFlight.find_by_ids(passenger, flight)
+    pass_flight.destroy
+    redirect_to flights_path
+  end
+end
